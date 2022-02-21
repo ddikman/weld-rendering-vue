a785f8d0c91eec51e52654a9fdfdde9f6b25e847
David Dikman <ddikman@gmail.com>
2022-02-21 19:01:38 +0900

    Added the base file and it can render

---
+++ definition.js
@@ -0,0 +1,130 @@
+export default {
+  "widgets": [
+    {
+      "type": "banner",
+      "title": "クーポンキャンペーン",
+      "data": {
+        "image_id": "767f9b6a-7f99-4a20-9fba-205f6122f679",
+        "target": "page=webview&url=https%3A%2F%2Ffacy.jp%2Fenjoy-facy-coupon%2F&title=クーポンキャンペーン"
+      }
+    },
+    {
+      "type": "local_recent_items",
+      "title": "recent items",
+      "data": {}
+    },
+    {
+      "type": "item_list",
+      "title": "今気になる春アウターは？",
+      "data": {
+        "items": [
+          {
+            "id": "a0135c37-dfce-498b-9824-d2eaf94e71a1",
+            "type": "item",
+            "name": "L2-B リバーシブル フライトブルゾン",
+            "image": "2f6434a6-b090-4808-a19a-801fb5bd36d1",
+            "price_with_tax": 23100.0,
+            "shop_id": "fb82f6e4-df9a-4f51-b80c-0d9d68967e30",
+            "brand": "EDIFICE",
+            "discount": null
+          },
+          {
+            "id": "69588f36-bce7-4813-8d01-3384d734c2f3",
+            "type": "item",
+            "name": "【Barbour / バブアー】OVERSIZE WAX BEAUFORT (ビューフォート)",
+            "image": "59da5187-bf7c-415e-8ce4-5822e4d1517a",
+            "price_with_tax": 60500.0,
+            "shop_id": "fb82f6e4-df9a-4f51-b80c-0d9d68967e30",
+            "brand": "EDIFICE",
+            "discount": null
+          },
+          {
+            "id": "d6d4cb91-ac21-4456-9ac6-3cb8220176e4",
+            "type": "item",
+            "name": "【CIOTA / シオタ】スビンコットンタイロッケンコート",
+            "image": "df59875e-ee99-43ce-82ad-9e6bc13d4f19",
+            "price_with_tax": 75900.0,
+            "shop_id": "4e507a25-fed3-4778-9a2e-470836ee26c7",
+            "brand": "JOURNAL STANDARD",
+            "discount": null
+          },
+          {
+            "id": "ccd4d439-d47d-4eaa-a601-b7b0378abcb1",
+            "type": "item",
+            "name": "【DAIWA PIER39/ ダイワ ピア39】別注 Tech West Side Cap Shoulder",
+            "image": "6e2fa2b7-79cb-4bf0-8f11-f93125c7af48",
+            "price_with_tax": 49500.0,
+            "shop_id": "4e507a25-fed3-4778-9a2e-470836ee26c7",
+            "brand": "JOURNAL STANDARD",
+            "discount": null
+          }
+        ]
+      }
+    },
+    {
+      "type": "shop_list",
+      "title": "渋谷エリアの人気ショップ",
+      "data": {
+        "items": [
+          {
+            "id": "4e507a25-fed3-4778-9a2e-470836ee26c7",
+            "name": "JOURNAL STANDARD 渋谷スクランブルスクエア店(Men`s)",
+            "logo": "659b8239-1ec2-400b-a12f-139f68cdb913"
+          },
+          {
+            "id": "fb82f6e4-df9a-4f51-b80c-0d9d68967e30",
+            "name": "EDIFICE スクランブルスクエア店",
+            "logo": "e11a8936-f4b7-4fdd-be76-06a852b792f7"
+          },
+          {
+            "id": "398d965c-cf56-479e-9892-00f4dbbd8cf9",
+            "name": "ADAM ET ROPE' 渋谷パルコ HOMME",
+            "logo": "1b917394-4cfb-4165-aada-f2cee29c2f0a"
+          },
+          {
+            "id": "38baeed0-94d9-40b6-8fc9-291f88b20c74",
+            "name": "ANCHOR",
+            "logo": "7fdbf2f9-dc8a-4aa5-b0cf-ab1f86a1ba7b"
+          },
+          {
+            "id": "6159e241-5542-4f7d-b00d-1d721133a7a2",
+            "name": "ADONUST MUSEUM",
+            "logo": "9c2d0bf0-19a2-4081-b29b-76ed726f85a0"
+          },
+          {
+            "id": "af9bdcbd-67ce-4ef4-b188-022d546a4a31",
+            "name": "ZABOU TOKYO",
+            "logo": "3f0d0070-bdf6-48ce-b0bc-061b977d395a"
+          },
+          {
+            "id": "bb9d3687-ffd0-4f0e-9712-96030ec0bbe4",
+            "name": "ニールズヤード レメディーズ渋谷ヒカリエ ShinQs店",
+            "logo": "d1b58015-37ab-4f42-9d2f-0b71bd36dae9"
+          },
+          {
+            "id": "60928a6c-90a0-4d63-945c-33ccc5e0980e",
+            "name": "NailBar Smart Style",
+            "logo": "8977f143-908f-43d4-9fc2-943345113aaa"
+          }
+        ]
+      }
+    },
+    {
+      "type": "banner",
+      "title": "417EDIFICE",
+      "data": {
+        "image_id": "b6dabed9-5330-499d-b90d-f15bc820a26c",
+        "target": "page=shop&shop_id=8e349736-2503-4c40-94e9-5441502584d4&title=417EDIFICE"
+      }
+    },
+    {
+      "type": "banner",
+      "title": "『NEHAN TOKYO』で見つける癒やしのバスギフト",
+      "data": {
+        "image_id": "f444a87d-caf7-46a6-91d5-2ac0ec4c6b4b",
+        "target": "page=webview&url=https%3A%2F%2Ffacy.jp%2Fnehan-tokyo-gift%2F&title=『NEHAN TOKYO』で見つける癒やしのバスギフト"
+      }
+    }
+  ]
+}
+
